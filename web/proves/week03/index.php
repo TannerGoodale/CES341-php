14e90d119f2962515ef48f013eae4f129a63b6aa
Tanner Goodale <49888850+TannerGoodale@users.noreply.github.com>
2020-05-09 19:19:04 -0500

    massive fix made

--- web/proves/week03/index.php
+++ web/proves/week03/index.php
@@ -17,31 +17,6 @@ switch ($action) {
   case 'getProdInfo':
     
      $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-    
-    /* switch ($id) {
-        case '1':
-            $prod = array ( "id"=>"1", "name"=>"Spring Apple", "disc"=>"A wonderful spring time herbal tea. Made from all natural dried apple, lemon peels, hibiscus flowers and other fresh friuts.", "price"=>"$4.00", "img-location"=>"assests/apple.jpg" );
-            $prodId = $prod['id'];
-            $prodName = $prod['name'];
-            $prodDisc = $prod['disc'];
-            $prodPrice = $prod['price'];
-            $img = $prod['img-location'];
-            // Build body
-            $prodInfo = "<div id='container'>";
-            $prodInfo .= "<h1>$prodName</h1>";
-            $prodInfo .= "<div class='side-by-side'>";
-            $prodInfo .= "<img src='$img'>";
-            $prodInfo .= "<p>$prodDisc</p>";
-            $prodInfo .= "</div>";
-            $prodInfo .= "<span>$prodPrice</span>";
-            $prodInfo .= "<form action='../index.php' method='GET'>";
-            $prodInfo .= "<input type='hidden' name='prodId' value='$prodId'>";
-            $prodInfo .= "<input type='hidden' name='action' value='add'>";
-            $prodInfo .= "<input name='submit' type='submit' value='Add To Cart'>";
-            $prodInfo .= "</form>";
-            $prodInfo .= "</div>";
-        exit;
-    } */
 
     $prodInfo = getProductInfoById($products, $id);
 
--- web/proves/week03/views/browse.php
+++ web/proves/week03/views/browse.php
@@ -13,7 +13,7 @@
 <body>
     <div id="container">
         <header>
-            <?php include $_SERVER['DOCUMENT_ROOT'].'modules/header.php';?>
+            <?php include '../modules/header.php';?>
         </header>
         <main>
             <section>
@@ -21,7 +21,7 @@
             </section>
         </main>
         <footer>
-        <?php include $_SERVER['DOCUMENT_ROOT']. 'modules/footer.php';?>
+        <?php include '../modules/footer.php';?>
         </footer>
     </div>
 </body>
