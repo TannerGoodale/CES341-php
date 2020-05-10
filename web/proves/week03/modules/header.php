5093c9de94c59b296613c0ea0ce8df11213476ce
Tanner Goodale <49888850+TannerGoodale@users.noreply.github.com>
2020-05-09 19:22:43 -0500

    logo fix

--- web/proves/week03/modules/header.php
+++ web/proves/week03/modules/header.php
@@ -1,3 +1,3 @@
-<img src="../assests/logo.png" alt="Company Logo">
+<img src="assests/logo.png" alt="Company Logo">
 <h2>Premium Herbal Teas</h2>
-<a href="../index.php?action=cart">Cart (<?php echo count($cartContent); ?>)</a>
+<a href="index.php?action=cart">Cart (<?php echo count($cartContent); ?>)</a>
