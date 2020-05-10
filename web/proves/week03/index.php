722283bc71c4c527d8edd40f2421d64ebaf7a24c
Tanner Goodale <49888850+TannerGoodale@users.noreply.github.com>
2020-05-09 20:35:05 -0500

    m

--- web/proves/week03/index.php
+++ web/proves/week03/index.php
@@ -7,6 +7,11 @@ require_once '../week03/library/fakedb.php';
 // Create or access a Session 
 session_start();
 
+// Create array if it does not yet exsist
+if(!isset($cartContent)){
+$cartContent = array();
+}
+
 // Create controll structure for dynamic site navigation
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
@@ -30,10 +35,6 @@ switch ($action) {
 
     $id = filter_input(INPUT_POST, 'prodId', FILTER_SANITIZE_NUMBER_INT);
 
-    if(!isset($cartContent)){
-        $cartContent = array();
-    }
-    
     array_push($cartContent, $id);
 
     $_SESSION['cartContent'] = $cartContent;
