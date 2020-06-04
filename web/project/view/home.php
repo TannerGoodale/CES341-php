c7f8972a2a95e9d45d6aebdc8229a65bcd424ac4
Tanner Goodale <49888850+TannerGoodale@users.noreply.github.com>
2020-06-04 11:07:22 -0500

    link fixes

--- web/project/view/home.php
+++ web/project/view/home.php
@@ -18,7 +18,7 @@
 
 </header>
 
-<main>
+<main class="center">
 
     <section>
         <h1>Welcome to SteeptClub</h1>
@@ -31,27 +31,27 @@
             <h3>Our Favorates for the Season</h3>
             <ul id="prod-display">
                 <li>
-                <a href='../products/index.php?action=getProdInfo&invId=6'><img src='../images/products/sweet-spring-herbal-tea-tn.jpg' alt='Image of Sweet Spring Blend on SteeptClub.com'></a>
+                <a href='products/index.php?action=getProdInfo&invId=6'><img src='/project/images/products/sweet-spring-herbal-tea-tn.jpg' alt='Image of Sweet Spring Blend on SteeptClub.com'></a>
                 <hr>
-                <a href='../products/index.php?action=getProdInfo&invId=6'><h4>Sweet Spring Blend</h4></a>
+                <a href='products/index.php?action=getProdInfo&invId=6'><h4>Sweet Spring Blend</h4></a>
                 <span>$4.00</span>
                 </li>
                 <li>
-                <a href='../products/index.php?action=getProdInfo&invId=1'><img src='../images/products/african-delight-loose-leaf-tea-tn.jpg' alt='Image of African Delight Blend on SteeptClub.com'></a>
+                <a href='products/index.php?action=getProdInfo&invId=1'><img src='/project/images/products/african-delight-loose-leaf-tea-tn.jpg' alt='Image of African Delight Blend on SteeptClub.com'></a>
                 <hr>
-                <a href='../products/index.php?action=getProdInfo&invId=1'><h4>African Delight Blend</h4></a>
+                <a href='products/index.php?action=getProdInfo&invId=1'><h4>African Delight Blend</h4></a>
                 <span>$4.00</span>
                 </li>
                 <li>
-                <a href='../products/index.php?action=getProdInfo&invId=7'><img src='../images/products/green-rooibos-loose-leaf-tea-tn.jpg' alt='Image of Pure Green Rooibos on SteeptClub.com'></a>
+                <a href='products/index.php?action=getProdInfo&invId=7'><img src='/project/images/products/green-rooibos-loose-leaf-tea-tn.jpg' alt='Image of Pure Green Rooibos on SteeptClub.com'></a>
                 <hr>
-                <a href='../products/index.php?action=getProdInfo&invId=7'><h4>Green Rooibos</h4></a>
+                <a href='products/index.php?action=getProdInfo&invId=7'><h4>Green Rooibos</h4></a>
                 <span>$3.00</span>
                 </li>
                 <li>
-                <a href='../products/index.php?action=getProdInfo&invId=2'><img src='../images/products/apple-loose-leaf-tea-tn.jpg' alt='Image of Apple Blend on SteeptClub.com'></a>
+                <a href='products/index.php?action=getProdInfo&invId=2'><img src='/project/images/products/apple-loose-leaf-tea-tn.jpg' alt='Image of Apple Blend on SteeptClub.com'></a>
                 <hr>
-                <a href='../products/index.php?action=getProdInfo&invId=2'><h4>Apple Blend</h4></a>
+                <a href='products/index.php?action=getProdInfo&invId=2'><h4>Apple Blend</h4></a>
                 <span>$3.00</span>
                 </li>
             </ul>
