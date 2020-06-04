ac14c813dbbc25adbcf4f3c165e766583af3330d
Tanner Goodale <49888850+TannerGoodale@users.noreply.github.com>
2020-06-04 11:00:14 -0500

    As far as I got

--- web/project/modules/footer.php
+++ web/project/modules/footer.php
@@ -2,5 +2,7 @@
 <p>&copy; SteeptClub, All rights reserved.</p>
     
 <p>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
+
+<h5>Note that this website is for testing and development, a user can not actually purchase any of the prodcuts displayed thereon.  purchasing features have been disabled and removed.</h5>
     
 <p>Last updated: <?php print date('j F, Y', getlastmod()) ?></p>
--- web/project/view/home.php
+++ web/project/view/home.php
@@ -23,7 +23,39 @@
     <section>
         <h1>Welcome to SteeptClub</h1>
         
-        <p>This is placeholder text<p>
+        <p>
+            Family owned and operated, we started SteeptClub in 2019 as a way to share one of our many passions, top quality herbal tea.
+        <p>
+
+        <section class="center">
+            <h3>Our Favorates for the Season</h3>
+            <ul id="prod-display">
+                <li>
+                <a href='../products/index.php?action=getProdInfo&invId=6'><img src='../images/products/sweet-spring-herbal-tea-tn.jpg' alt='Image of Sweet Spring Blend on SteeptClub.com'></a>
+                <hr>
+                <a href='../products/index.php?action=getProdInfo&invId=6'><h4>Sweet Spring Blend</h4></a>
+                <span>$4.00</span>
+                </li>
+                <li>
+                <a href='../products/index.php?action=getProdInfo&invId=1'><img src='../images/products/african-delight-loose-leaf-tea-tn.jpg' alt='Image of African Delight Blend on SteeptClub.com'></a>
+                <hr>
+                <a href='../products/index.php?action=getProdInfo&invId=1'><h4>African Delight Blend</h4></a>
+                <span>$4.00</span>
+                </li>
+                <li>
+                <a href='../products/index.php?action=getProdInfo&invId=7'><img src='../images/products/green-rooibos-loose-leaf-tea-tn.jpg' alt='Image of Pure Green Rooibos on SteeptClub.com'></a>
+                <hr>
+                <a href='../products/index.php?action=getProdInfo&invId=7'><h4>Green Rooibos</h4></a>
+                <span>$3.00</span>
+                </li>
+                <li>
+                <a href='../products/index.php?action=getProdInfo&invId=2'><img src='../images/products/apple-loose-leaf-tea-tn.jpg' alt='Image of Apple Blend on SteeptClub.com'></a>
+                <hr>
+                <a href='../products/index.php?action=getProdInfo&invId=2'><h4>Apple Blend</h4></a>
+                <span>$3.00</span>
+                </li>
+            </ul>
+            </section>
     
     </section>
 
